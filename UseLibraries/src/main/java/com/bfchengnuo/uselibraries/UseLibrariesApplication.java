6b7f5a85648f69353f7783fc1a4f3a541f3d558d
bfchengnuo <bfchengnuo@gmail.com>
2019-04-21 19:46:45 +0800

    init

---
+++ UseLibraries/src/main/java/com/bfchengnuo/uselibraries/UseLibrariesApplication.java
@@ -0,0 +1,13 @@
+package com.bfchengnuo.uselibraries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseLibrariesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UseLibrariesApplication.class, args);
+    }
+
+}
---
+++ UseLibraries/src/test/java/com/bfchengnuo/uselibraries/UseLibrariesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bfchengnuo.uselibraries;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UseLibrariesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
